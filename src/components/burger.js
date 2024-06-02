cb106c5acb6649525dd7b5c61271e684fa2e5a04
lilisindrianiii <lilisindriani.blog@gmail.com>
2024-06-02 21:47:28 +0700

    day 10 updated

---
+++ src/components/burger.js
@@ -0,0 +1,55 @@
+import { Component } from "react";
+
+import Burgerr from './../assets/images/burger.png';
+import WBurger from './../assets/images/white_burger.png';
+
+class Burgers extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      burger:false
+    }
+  }
+  hamburger = () => {
+    if(this.state.burger === true){
+      this.setState({burger:false})
+    }else{
+      this.setState({burger:true})
+    }
+  }
+  render(){
+    return(
+      <div >
+        <>
+          {this.state.burger == false &&
+            <a className="burger">
+            <img src={Burgerr } className="image_burger" onClick={this.hamburger}/>
+            <div className="burger_write2">Menu</div>
+            </a>
+          }
+        </>
+        {this.state.burger &&
+          <div className="burger" onClick={this.hamburger}>
+            <div className="color">
+              <img src={Burgerr} className="image_burger2"/>
+              <div className="write_menu2">
+              <a href="#home" className="write2"> Home</a>
+            </div>
+            <div className="write_menu2">
+              <a href="#education" className="write2"> Ediucation</a>
+            </div>
+            <div className="write_menu2">
+              <a href="#skills" className="write2"> Skills</a>
+            </div>
+            <div className="write_menu2">
+              <a href="#moreinfo"  className="write2"> More Info</a>
+            </div>
+            </div>
+            <div></div>            
+          </div>
+        }
+      </div>
+    )
+  }
+}
+export default Burgers;
--- src/screens/home.js
+++ src/screens/home.js
@@ -7,6 +7,10 @@ import Girl2 from '../assets/images/girl2.png'
 import Creative from '../assets/images/creative.png'
 import Thinker from '../assets/images/think.png'
 import Team from '../assets/images/team.png'
+import Burger from '../components/burger.js'
+import Melon from '../assets/images/melon.png'
+import Panah from '../assets/images/arrow.png'
+import Flower from '../assets/images/blueFlower.png'
 
 class Home extends Component {
   constructor(props) {
@@ -20,38 +24,39 @@ class Home extends Component {
 
       <div id="home" className="home">
         <div className='menu1'>
-            <div className='logo'>
-              <img src={Logo} className='logo_img' />
-            </div>
-
-            <div className='cover_menu1'>
-              <div className='write1_menu1'>
-                <a href="#home" className='writeM1'> Home</a>
-                <a href="#Ame" className='writeM1'> About Me</a>
-                <a href="#Edu" className='writeM1'> Education</a>
-                <a href="#Cme" className='writeM1'> Contact Me</a>
-              </div>
-              <div className='write2_menu1'>
-                {/* <a href="#FL" className='writeM2'> </a> */}
-                <a href="#Pro" className='writeM2'> Project</a>
-                <a href="#skills" className='writeM2'> Skills</a>
-
-              </div>
-
-            </div>
+          <div className='logo'>
+            <img src={Logo} className='logo_img' />
           </div>
-        <div className='home_cover'>
           
+          <div className='cover_menu1'>
+           
+            <div className='write1_menu1'>
+              <a href="#home" className='writeM1'> Home</a>
+              <a href="#Ame" className='writeM1'> About Me</a>
+              <a href="#Edu" className='writeM1'> Education</a>
+              <a href="#Cme" className='writeM1'> Contact Me</a>
+            </div>
+            <div className='write2_menu1'>
+              {/* <a href="#FL" className='writeM2'> </a> */}
+              <a href="#Pro" className='writeM2'> Project</a>
+              <a href="#skills" className='writeM2'> Skills</a>
+
+            </div>
+
+          </div>
+        </div>
+        <div className='home_cover'>
+        <Burger />
           {/* <Contact /> */}
           <div className='cover_me'>
-            <div className='write_me'> 
+            <div className='write_me'>
               <button className='button_me_title'>button</button>
-              <div className='write_me1'> Hi I am 
-               
+              <div className='write_me1'> Hi I am
+
                 <div className='img_title_write_me_cover'>
-                  <img src={Girl2} className='img_title_write_me'/>
-                  <img src={Girl2} className='img_title_write_me'/>
-                  <img src={Girl2} className='img_title_write_me'/>
+                  <img src={Girl2} className='img_title_write_me' />
+                  <img src={Girl2} className='img_title_write_me' />
+                  <img src={Girl2} className='img_title_write_me' />
                 </div>
               </div>
               <div className='write_me1'> Lilis Indriani</div>
@@ -64,71 +69,97 @@ class Home extends Component {
               <div className='image_me'>
                 <img src={Lilis} className='image_me_url' />
                 <div className='wrap_img_write'>
-                <img src={Thinker} className='img_inside_write' />
+                  <img src={Thinker} className='img_inside_write' />
                   <div className='img_write'>Critical thinker</div>
                 </div>
                 <div className='wrap_img_write2'>
-                <img src={Team} className='img_inside_write2' />
+                  <img src={Team} className='img_inside_write2' />
                   <div className='img_write2'>Work on team</div>
                 </div>
                 <div className='wrap_img_write3'>
                   <img src={Creative} className='img_inside_write3' />
                   <div className='img_write3'>Creative</div>
                 </div>
+                <div className='image_me_write'>
+                  <div className='write_image_me'>Lets check up on me, scrolling down and you gonna find me there</div>
+                </div>
               </div>
-              <div className='image_me_write'>
-                <div className='write_image_me'>Lets check up on me, scrolling down and you gonna find me there</div>
-              </div>
+
             </div>
           </div>
 
-        
-        <div className='cover'>
-        < div className="grey">
-            {/* <Color color="yellow"> hellooo</Color> */}
-            <div className="wrap_image_grey">
-              {/* <img src={Gimp} className="image_tools2" /> */}
-              <div className="wrap_ww_grey">
-                <div className="write_grey">Gimp</div>
-                <div className="write_grey ">Intermediet</div>
+
+          <div className='cover'>
+            < div className="grey">
+              {/* <Color color="yellow"> hellooo</Color> */}
+              <div className="wrap_image_grey">
+
+                <div className="wrap_ww_grey">
+                  <div className="write_grey">Gimp</div>
+                  <div className='cover_shape_grey'>
+                    <div className='shape_grey'>hello</div>
+                    <div className='shape_grey'>ima creative</div>
+                    <div className='shape_grey'>work on team</div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          < div className="yellow">
-            {/* <Color color="yellow"> hellooo</Color> */}
-            <div className="wrap_image_yellow">
-              {/* <img src={Gimp} className="image_tools2" /> */}
-              <div className="wrap_ww_yellow">
-                <div className="write_yellow">work on team</div>
-                <div className="write_yellow2 ">i i used to work on team i am able to bring you to my home</div>
+            < div className="yellow">
+              {/* <Color color="yellow"> hellooo</Color> */}
+              <div className="wrap_image_yellow">
+
+
+                <div className="wrap_ww_yellow">
+                  <div className='cover_write_arrow'>
+                    <div className="write_yellow">work on team</div>
+                    <div className='cover_arrow'>
+                      <img src={Panah} className="image_yellow" />
+                    </div>
+                  </div>
+                  <div className="write_yellow2 ">i i used to
+                  <div className='write_yellow23'> work on team</div>
+                   i am able to bring you to my home</div>
+                </div>
               </div>
             </div>
-          </div>
-          
-          < div className="grey">
-            {/* <Color color="yellow"> hellooo</Color> */}
-            <div className="wrap_image_grey">
-              {/* <img src={Gimp} className="image_tools2" /> */}
-              <div className="wrap_ww_grey">
-                <div className="write_grey">Critical thinker</div>
-                <div className="write_grey2 ">i am critical thinker because i am able to bring you to my home</div>
+
+            < div className="grey2">
+              {/* <Color color="yellow"> hellooo</Color> */}
+              <div className="wrap_image_grey2">
+
+                <div className="wrap_ww_grey2">
+                  <div className="write_grey2">i am critical thinker because i am able to bring you to my home</div>
+                  {/* <div className="write_grey22 ">i am critical thinker because i am able to bring you to my home</div> */}
+
+                </div>
+                <div className='cover_fm'>
+                  <img src={Flower} className="image_grey_flower" />
+                  <img src={Melon} className="image_grey_melon" />
+
+                </div>
+
               </div>
             </div>
-          </div>
-          < div className="blue">
-            {/* <Color color="yellow"> hellooo</Color> */}
-            <div className="wrap_image_tools2">
-              {/* <img src={Gimp} className="image_tools2" /> */}
-              <div className="wrap_ww_tools2">
-                <div className="write_tools2">Creative</div>
-                <div className="write_tools22 ">i am creative because i am able to bring you to my home</div>
+            < div className="blue">
+              {/* <Color color="yellow"> hellooo</Color> */}
+              <div className="wrap_image_tools2">
+                {/* <img src={Gimp} className="image_tools2" /> */}
+                <div className="wrap_ww_tools2">
+                <div className='cover_write_arrow2'>
+                    <div className="write_tools2">Creative</div>
+                    <div className='cover_arrow2'>
+                      <img src={Panah} className="image_blue" />
+                    </div>
+                  </div>
+                  <div className="write_tools23 ">450+</div>
+                </div>
               </div>
             </div>
+
+
           </div>
-          
+
         </div>
-
-      </div>
       </div>
 
 
